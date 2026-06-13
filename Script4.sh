73cfcbf0ee8dc5128aac0343a4e22781c48a3282
Tarun <tarunsinghrajput55@gmail.com>
2026-06-13 19:22:54 +0530

    Trying again

--- Script4.sh
+++ Script4.sh
@@ -5,3 +5,4 @@ Batch=25
 PROFESSION=AWS/DevSecOps/Azure DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
 echo "We are learning $PROFESSION in $Class class, Batch number is $Batch"
+echo "This is the end of the script"
